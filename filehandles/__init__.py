f8782baadf6bb7e8c70a18847e8245aa80df49bf
andreysmelter <andrey.smelter@gmail.com>
2018-03-26 12:02:00 -0400

    Set default logging handler.

--- filehandles/__init__.py
+++ filehandles/__init__.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
+from .filehandles import filehandles
+
+
 __version__ = "0.1.0"
 
-from .filehandles import filehandles
+# Setting default logging handler
+try:  # Python 2/3 compatibility code
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
