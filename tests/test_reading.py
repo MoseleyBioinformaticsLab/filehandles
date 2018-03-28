881d47801fc017df776a4076555907a4535fdc2d
Andrey Smelter <andrey.smelter@gmail.com>
2018-03-28 01:14:35 -0400

    Added tests to include pattern parameter.

--- tests/test_reading.py
+++ tests/test_reading.py
@@ -34,3 +34,26 @@ def test_reading(files_source):
     for fh in filehandles.filehandles(files_source, verbose=True, cif='text/cif'):
         mock = Mock(fh)
         assert mock.first_line.strip() in ('data_2RPV', b'data_2RPV')
+
+
+@pytest.mark.parametrize('files_source', [
+    'tests/example_data/2rpv.cif',
+    'tests/example_data/2rpv',
+    'tests/example_data/2rpv.cif.gz',
+    'tests/example_data/2rpv.cif.bz2',
+    'tests/example_data/directory',
+    'tests/example_data/archive.zip',
+    'tests/example_data/archive.tar.gz',
+    'tests/example_data/archive.tar.bz2',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/2rpv.cif',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/2rpv',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/2rpv.cif.gz',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/2rpv.cif.bz2',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/archive.zip',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/archive.tar.gz',
+    'https://raw.githubusercontent.com/MoseleyBioinformaticsLab/filehandles/master/tests/example_data/archive.tar.bz2'
+])
+def test_reading_with_pattern(files_source):
+    for fh in filehandles.filehandles(files_source, verbose=True, cif='text/cif', pattern='[\w\d\s]+'):
+        mock = Mock(fh)
+        assert mock.first_line.strip() in ('data_2RPV', b'data_2RPV')
